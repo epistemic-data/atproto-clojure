3b5a0599bceaf8cefb8a4e49bd0deb9f3ac34241
Chase Lambert <chaselambert@gmail.com>
2024-12-06 12:49:14 -0800

    minor tweaks

--- src/net/gosha/atproto/fetch.clj
+++ src/net/gosha/atproto/fetch.clj
@@ -7,19 +7,19 @@
    [net.gosha.atproto.core   :as core]))
 
 (defn fetch-author-feed
-  "GET request for user's posts using existing client infrastructure.
+  "GET request for user's posts.
    Fetches and filters posts from the last N days."
   [actor & {:keys [limit cursor days-ago]
             :or   {limit    50
                    days-ago 7}}]
-  (let [cutoff   (jt/local-date-time (jt/minus (jt/local-date) (jt/days days-ago)))
-        params   {:actor  actor
-                  :limit  limit
-                  :cursor cursor}
-        response (:body (client/get-req
-                          "/xrpc/app.bsky.feed.getAuthorFeed"
-                          {:params params}))
-        posts    (get-in response [:feed])
+  (let [cutoff    (jt/local-date-time (jt/minus (jt/local-date) (jt/days days-ago)))
+        params    {:actor  actor
+                   :limit  limit
+                   :cursor cursor}
+        response  (:body (client/get-req
+                           "/xrpc/app.bsky.feed.getAuthorFeed"
+                           {:params params}))
+        posts     (get-in response [:feed])
         in-range? (fn [post]
                    (let [post-date (-> post 
                                      (get-in [:post :indexedAt])
@@ -40,11 +40,26 @@
   (client/authenticate!)
   
   (fetch-author-feed "chaselambert.dev" :limit 5)
-  (fetch-author-feed "vanderhart.net"   :days-ago 1 :limit 5)
+  (fetch-author-feed "vanderhart.net"   :days-ago 2 :limit 3)
   (fetch-author-feed "vanderhart.net"   :limit 5)
+
+  ;; First request gets initial batch and a cursor
+  (let [first-response (fetch-author-feed "vanderhart.net" :limit 10)
+        cursor        (:cursor first-response)]
+    
+    ;; Use that cursor to get next batch
+    (when cursor
+      (fetch-author-feed "vanderhart.net" 
+                        :limit 10 
+                        :cursor cursor)))
   
-  ;; Debug: Look at dates of returned posts
-  (-> (fetch-author-feed "vanderhart.net" :days-ago 3)
-      :feed
-      (->> (mapv :indexedAt))
-      clojure.pprint/pprint))
+  (let [sample-date "2024-12-04T23:35:03.023Z"
+        cutoff      (jt/local-date-time (jt/minus (jt/local-date) (jt/days 1)))]
+    {:parsed-date (-> sample-date jt/instant (jt/local-date-time "UTC"))
+     :cutoff      cutoff
+     :in-range?   (jt/not-before? 
+                    (-> sample-date jt/instant (jt/local-date-time "UTC")) 
+                    cutoff)})
+  
+  
+  ,)
