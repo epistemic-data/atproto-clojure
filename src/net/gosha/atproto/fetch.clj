18febbffa90555fb86054d4e6f2b478f2696406d
Chase Lambert <chaselambert@gmail.com>
2024-12-05 16:40:31 -0800

    different, simpler approach but still getting an api failure right at the end

--- src/net/gosha/atproto/fetch.clj
+++ src/net/gosha/atproto/fetch.clj
@@ -1,88 +1,33 @@
 (ns net.gosha.atproto.fetch
   (:require
-   [net.gosha.atproto.client :as client]
+   [org.httpkit.client       :as http]
+   [clojure.data.json        :as json]
    [clojure.java.io          :as io]
    [aero.core                :as aero]
-   [java-time.api            :as jt]))
+   [java-time.api            :as jt]
+   [net.gosha.atproto.core   :as core]
+   [net.gosha.atproto.client :as client]))
 
-(defn get-user-posts
-  "Fetch posts from a user within the last N days.
-   Returns a lazy sequence of posts to handle large result sets.
-   
-   user     - The handle or DID of the user
-   days-ago - Number of days back to fetch (default 7)
-   limit    - Max posts per request (default 100)"
-  [user & {:keys [days-ago limit]
-           :or   {days-ago 7
-                  limit    100}}]
-  (let [cutoff (jt/minus (jt/local-date) (jt/days days-ago))]
-    (loop [posts   []
-           cursor  nil]
-      (let [response (client/get-req 
-                      "/xrpc/app.bsky.feed.getAuthorFeed"
-                      {:params {:actor  user
-                                :cursor cursor 
-                                :limit  limit}})
-            new-posts (get-in response [:feed :posts])
-            earliest  (some-> new-posts 
-                            last 
-                            :indexed-at
-                            (jt/local-date "yyyy-MM-dd'T'HH:mm:ss.SSSX"))]
-        (cond
-          (empty? new-posts)           posts  ; No more posts
-          (jt/before? earliest cutoff) posts  ; Past our date range
-          :else (recur (into posts new-posts)
-                      (get-in response [:cursor])))))))
-
-(defn get-list-posts
-  "Fetch posts from all users in a list within the last N days.
-   Returns a lazy sequence of posts to handle large result sets.
-   
-   list-uri - The URI of the list
-   days-ago - Number of days back to fetch (default 7)
-   limit    - Max posts per request (default 100)"
-  [list-uri & {:keys [days-ago limit]
-               :or   {days-ago 7
-                      limit    100}}]
-  (let [cutoff (jt/minus (jt/local-date) (jt/days days-ago))]
-    (loop [posts   []
-           cursor  nil]
-      (let [response (client/get-req 
-                      "/xrpc/app.bsky.feed.getListFeed"
-                      {:params {:list   list-uri
-                                :cursor cursor
-                                :limit  limit}})
-            new-posts (get-in response [:feed :posts])
-            earliest  (some-> new-posts 
-                            last 
-                            :indexed-at
-                            (jt/local-date "yyyy-MM-dd'T'HH:mm:ss.SSSX"))]
-        (cond
-          (empty? new-posts)           posts  ; No more posts
-          (jt/before? earliest cutoff) posts  ; Past our date range
-          :else (recur (into posts new-posts)
-                      (get-in response [:cursor])))))))
+(defn fetch-author-feed
+  "GET request for a user's posts with authentication"
+  [actor & {:keys [limit cursor]
+            :or   {limit 50}}]
+  (let [params {:actor  actor
+                :limit  limit
+                :cursor cursor}]
+    (client/get-req 
+     "/xrpc/app.bsky.feed.getAuthorFeed"
+     {:params params})))
 
 (comment
-  ;; 1. Check if config loaded
-  (println "Current config:" @net.gosha.atproto.core/config)
-  
-  ;; 2. Initialize with config
+  ;; First initialize and authenticate using existing setup
   (let [dev-config (aero/read-config (io/resource "dev-config.edn"))]
-    (println "Loaded dev config:" dev-config)
-    (net.gosha.atproto.core/init 
+    (core/init 
      {:base-url     "https://bsky.social"
       :username     (:username dev-config)
       :app-password (:password dev-config)}))
   
-  ;; 3. Check config again
-  (println "Updated config:" @net.gosha.atproto.core/config)
+  (client/authenticate!)
   
-  ;; 4. Authenticate
-  (net.gosha.atproto.client/authenticate!)
-  
-  ;; 5. Check config has auth token
-  (println "Auth token present?" (boolean (:auth-token @net.gosha.atproto.core/config)))
-  
-  ;; 6. Now try getting posts
-  (get-user-posts "chaselambert.dev" :days-ago 2 :limit 50))
+  ;; Now try the fetch
+  (fetch-author-feed "chaselambert.dev" :limit 10))
