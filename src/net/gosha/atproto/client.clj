ab4afdd40d5c4b6bd13a527c8ad872f0d50fb144
Chase Lambert <chaselambert@gmail.com>
2024-12-05 17:30:48 -0800

    basic fetch functionality working! Now adding time element

--- src/net/gosha/atproto/client.clj
+++ src/net/gosha/atproto/client.clj
@@ -1,6 +1,7 @@
 (ns net.gosha.atproto.client
   (:require [org.httpkit.client :as http]
             [charred.api :as json]
+            [clojure.pprint :refer [pprint]]
             [net.gosha.atproto.core :as core]))
 
 (defn request
