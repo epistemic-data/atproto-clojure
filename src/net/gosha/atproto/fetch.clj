5eced6396ec32097e3a3a1277d87201ec0b5b7c4
Chase Lambert <chaselambert@gmail.com>
2024-12-16 21:17:12 -0500

    able to get author feed (without auth) using openapi json spec and martian

--- src/net/gosha/atproto/fetch.clj
+++ src/net/gosha/atproto/fetch.clj
@@ -1,26 +1,44 @@
 (ns net.gosha.atproto.fetch
   (:require
    [martian.core :as martian]
-   [clojure.java.io :as io]))
+   [martian.httpkit :as martian-http]))
 
-(def api-url "https://public.api.bsky.app/xrpc/")
+(def openapi-url 
+  "https://raw.githubusercontent.com/bluesky-social/bsky-docs/refs/heads/main/atproto-openapi-types/spec/api.json")
 
-(def openapi-spec
-  (-> (io/resource "openapi/api.json")
-      slurp))
+(def client 
+  (martian-http/bootstrap-openapi
+    openapi-url
+    {:server-url "https://public.api.bsky.app"}))
 
-(def client
-  (martian/bootstrap-openapi api-url "resources/openapi/api.json"))
-
-;; (def client
-;;   (martian/bootstrap-openapi api-url openapi-spec))
+(defn author-feed
+  "Fetch an author's feed from Bluesky.
+   Required params:
+   - actor: The handle of the user (e.g. 'user.bsky.social')
+   Optional params:
+   - limit: Number of posts (1-100, default 50)
+   - cursor: Pagination cursor
+   - filter: Type of posts to include
+   - include-pins: Whether to include pinned posts"
+  [actor & [opts]]
+  (:body @(martian/response-for client 
+                              :app.bsky.feed.get-author-feed 
+                              (merge {:actor actor} opts))))
 
 (comment
-  (println openapi-spec)
+  ;; Basic feed fetch
+  (author-feed "atproto.com")
   
-  (martian/explore client) ;; []
+  ;; With options
+  (author-feed "atproto.com" 
+               {:limit        10
+                :filter       "posts_no_replies"
+                :includePins  true})
   
-  (martian/response-for client 
-                       :app.bsky.feed.getAuthorFeed 
-                       {:actor "chaselambert.dev"})
+  ;; Get profile info (with proper dereferencing)
+  (-> (martian/response-for client 
+                          :app.bsky.actor.get-profile 
+                          {:actor "atproto.com"})
+      deref
+      :body)
   ,)
